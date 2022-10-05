7b8c10d8df2a7de4016558b2cdeea2cf90cec0c2
Rando Õispuu <rando@veebisepad.ee>
2022-10-05 11:22:56 +0300

    Rename page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 definePageMeta({
-    layout: "home",
+  layout: "home",
 });
 </script>
 <template>
-    <div class="flex-1 flex items-center justify-center 
-
-    bg-[url('./assets/images/bg-fog.png')]
-    bg-cover bg center bg-no-repeat"
-    >
-        <h1 class="text-6xl font-serif text-red-600"></h1>
-        <h2></h2>
-        Meelespea
-    </div>
+  <div
+    class="flex-1 flex items-center justify-center bg-[url('./assets/images/bg-fog.png')] bg-cover bg center bg-no-repeat"
+  >
+    <h1 class="text-6xl font-serif text-red-600"></h1>
+    <h2></h2>
+    Meelespea
+  </div>
 </template>
-
-
