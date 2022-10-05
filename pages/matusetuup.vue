d58ddc15077c9478ff0423c941cffc84a7087462
Rando Õispuu <rando@veebisepad.ee>
2022-10-05 15:44:29 +0300

    Add button, Add toggle

--- pages/matusetuup.vue
+++ pages/matusetuup.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+const tyyp = ref(true);
 
+const toggleType = (val) => {
+  tyyp.value = val;
+};
 </script>
 <template>
-    <div class="flex-1 flex items-center justify-center"> 
-       
+  <div class="flex-1 flex justify-center">
+    <div class="h-10 flex items-center bg-gray-100 rounded">
+      <BaseButton
+        :class="{ 'border border-black bg-cyan-800 text-white': tyyp }"
+        @click="toggleType(true)"
+      >
+        Tuhastamine
+      </BaseButton>
+      <BaseButton
+        :class="{ 'border border-black bg-cyan-800 text-white': !tyyp }"
+        @click="toggleType(false)"
+      >
+        Kirstumatus
+      </BaseButton>
     </div>
+  </div>
 </template>
