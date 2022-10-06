c532ff69d54143075fc602aef062458af5d81807
elmipuna <101389574+elmipuna@users.noreply.github.com>
2022-10-06 14:39:16 +0300

    info button error

---
+++ components/PersonSimple.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex text-black-500 text-lg py-3 px-2 shadow justify-between "> 
+        <slot></slot>
+        <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600" checked />
+    </div>
+</template>
